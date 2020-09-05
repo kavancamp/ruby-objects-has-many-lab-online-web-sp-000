a3a02d2ca9645b14475bfc1cfa790755ed63c9c0
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 00:25:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,29 @@
-
 class Artist
-  attr_accessor :name, :song
+
+  attr_accessor :name, :songs
+
   @@song_count = 0
 
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
-  end
-  def self.song_count
-    @@song_count
-  end
-
-  def songs
-    @songs
+    @songs = []
   end
 
   def add_song(song)
-    @songs << song_name
-    song_name.artist = self
+    @songs << song
+    song.artist = self
     @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @song << song
-      song.artist = self
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
     @@song_count += 1
   end
 
-
+  def self.song_count
+    @@song_count
+  end
 
 end
