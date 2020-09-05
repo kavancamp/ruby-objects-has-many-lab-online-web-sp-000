14bbf889b9ee7e4b8361d9a7fae7015f5715b951
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 00:49:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ class Artist
     @@song_count += 1
   end
   def songs
-    @songs
-end
+    Song.all.select {|song| song.artist == self}
+  end
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
