1a103f4e87b3a40374f7f3ecc2c4bbfedd2193d1
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 00:19:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+
   @@post_count = 0
 
   def initialize(name)
@@ -11,9 +12,13 @@ class Author
     @posts
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
+  def self.post_count
+    @@post_count
+  end
+
+  def add_post(post_title)
+    @posts << post_title
+    post_title.author = self
     @@post_count += 1
   end
 
@@ -23,9 +28,4 @@ class Author
     post.author = self
     @@post_count += 1
   end
-
-  def self.post_count
-    @@post_count
-  end
-
 end 
